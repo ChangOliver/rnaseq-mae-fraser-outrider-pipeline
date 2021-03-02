80c397f01ca9b60fc624a35ef3f0503de6f88225
ChangOliver <b06902119@ntu.edu.tw>
2021-03-03 01:16:41 +0800

    IO

--- Fraser/Fraser.R
+++ Fraser/Fraser.R
@@ -1,19 +1,50 @@
-suppressMessages(library(FRASER))
-suppressMessages(library(dplyr))
-suppressMessages(library(stringr))
+# argument parsing --------------------------------------------------------
+library(optparse)
 
-# take arguments (dataDir)
-args <- commandArgs(trailingOnly = TRUE)
-dataDir <- args[[1]]
+option_list = list(
+  make_option(c("-i", "--input"), type="character", default=NULL, 
+              help="input bam directory path", metavar="character"),
+  make_option(c("-o", "--output"), type="character", default=NULL, 
+              help="output directory path", metavar="character")
+  make_option(c("-t", "--tmp"), type="character", default=NULL, 
+              help="tmp directory path", metavar="character")
+)
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+opt$input <- ifelse(substr(opt$input, nchar(opt$input), nchar(opt$input))=='/', opt$input, paste0(opt$input,'/'))
+opt$output <- ifelse(substr(opt$output, nchar(opt$output), nchar(opt$output))=='/', opt$output, paste0(opt$output,'/'))
+opt$tmp <- ifelse(substr(opt$tmp, nchar(opt$tmp), nchar(opt$tmp))=='/', opt$tmp, paste0(opt$tmp,'/'))
+
+if (is.null(opt$input) | is.null(opt$output) | is.null(opt$tmp)){
+  print_help(opt_parser)
+  stop("Arguments must be supplied.", call.=FALSE)
+}
+
+if (!file_test("-d", opt$input)){
+  print_help(opt_parser)
+  stop("Input must be a directory.", call.=FALSE)
+}
+
+if (!file_test("-d", opt$output)){
+  dir.create(opt$output)
+}
+
+if (!file_test("-d", opt$tmp)){
+  dir.create(opt$tmp)
+}
+
+# main --------------------------------------------------------------------
+library(FRASER)
+library(dplyr)
+library(stringr)
 
 # extract bam files
-bam.all <- list.files(path = dataDir, pattern = "dedup.bam$", all.files = TRUE, recursive = TRUE)
-bam.all <- bam.all[grep("1.ALIGNMENT", bam.all)]
+bam.all <- list.files(path = opt$input, pattern = "dedup.bam$", all.files = TRUE, recursive = TRUE)
 
 # create file table
-dataInfo <- data.table(sampleID = str_match(bam.all, "1.ALIGNMENT/(.*?).sorted.dedup.bam")[,2], bamFile = paste0(dataDir, '/', bam.all), pairedEnd=TRUE)
+dataInfo <- data.table(sampleID = str_match(bam.all, "(.*?).sorted.dedup.bam")[,2], bamFile = paste0(opt$input, bam.all), pairedEnd=TRUE)
 
-settings <- FraserDataSet(colData=dataInfo, workingDir='all_wkdir')
+settings <- FraserDataSet(colData=dataInfo, workingDir=opt$tmp)
 register(MulticoreParam(workers=min(10, multicoreWorkers())))
 fds <- countRNAData(settings)
 
@@ -31,23 +62,21 @@ fds <- annotateRanges(fds)
 # get results: we recommend to use an FDR cutoff 0.05
 res <- results(fds, zScoreCutoff=NA, padjCutoff=0.05, deltaPsiCutoff=0.3)
 
-samples <- str_sort(unique(res$sampleID))
-savePath <- paste0(dataDir, "/", str_match(bam.all, "(.*)/1.ALIGNMENT/.*")[,2])
-# dir.create("Fraser_results", showWarnings = FALSE)
+# output results by sample
+samples <- unique(res$sampleID)
+samples <- samples[!grepl("Control", samples)]
 for (s in c(1:length(samples))){
   res_s <- res[res$sampleID == samples[s]] %>% replace(is.na(.), ".")
-  dir.create(paste0(savePath[s], "/21.Fraser"), showWarnings = FALSE)
-  write.csv(res_s, paste0(savePath[s], "/21.Fraser/", samples[s], '.FRASER.result.csv'), row.names=FALSE)
+  write.csv(res_s, paste0(opt$output, samples[s], '.FRASER.result.csv'), row.names=FALSE)
 }
 
-saveFraserDataSet(fds, dir='all_wkdir', name="Data_Analysis")
-
-# images
+# output graphs --------------------------------------------------------------------
 types = c("theta", "psi5", "psi3")
+dir.create(paste0(opt$output, "graphs"))
 
 for (s in c(1:length(samples))){
-  path = paste0(savePath[s], "/21.Fraser/graphs/Volcano/")
-  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  path = paste0(opt$output, "graphs/Volcano/")
+  dir.create(path)
   for (type in types){
     png(file=paste0(path, samples[s], ".Volcano.", type, ".png"))
     print(plotVolcano(fds, sampleID=samples[s], type=type))
@@ -57,12 +86,12 @@ for (s in c(1:length(samples))){
 
 for (s in c(1:length(samples))){
   extract <- res[res$sampleID == samples[s]]
-  QQpath = paste0(savePath[s], "/21.Fraser/graphs/QQ/")
-  EXpath = paste0(savePath[s], "/21.Fraser/graphs/Expression/")
-  POpath = paste0(savePath[s], "/21.Fraser/graphs/Prediction_vs_Observation/")
-  dir.create(QQpath, recursive = TRUE, showWarnings = FALSE)
-  dir.create(EXpath, recursive = TRUE, showWarnings = FALSE)
-  dir.create(POpath, recursive = TRUE, showWarnings = FALSE)
+  QQpath = paste0(opt$output, "graphs/QQ/")
+  EXpath = paste0(opt$output, "graphs/Expression/")
+  POpath = paste0(opt$output, "graphs/Prediction_vs_Observation/")
+  dir.create(QQpath)
+  dir.create(EXpath)
+  dir.create(POpath)
 
   for (i in c(1:length(extract))){
     if (is.na(extract[i]$hgncSymbol@values)){
@@ -79,15 +108,15 @@ for (s in c(1:length(samples))){
       dev.off()
     }
     else{
-      png(file=paste0(QQpath, samples[s], ".QQ-", i, ".", extract[i]$hgncSymbol, ".", extract[i]$type, ".png"))
+      png(file=paste0(QQpath, samples[s], ".QQ-", i, ".", extract[i]$type, ".", extract[i]$hgncSymbol, ".png"))
       print(plotQQ(fds, result=extract[i]))
       dev.off()
 
-      png(file=paste0(EXpath, samples[s], ".EX-", i, ".", extract[i]$hgncSymbol, ".", extract[i]$type, ".png"))
+      png(file=paste0(EXpath, samples[s], ".EX-", i, ".", extract[i]$type, ".", extract[i]$hgncSymbol, ".png"))
       print(plotQQ(fds, result=extract[i]))
       dev.off()    
 
-      png(file=paste0(POpath, samples[s], ".PvO-", i, ".", extract[i]$hgncSymbol, ".", extract[i]$type, ".png"))
+      png(file=paste0(POpath, samples[s], ".PvO-", i, ".", extract[i]$type, ".", extract[i]$hgncSymbol, ".png"))
       print(plotExpectedVsObservedPsi(fds, result=extract[i]))
       dev.off()      
     }
