e999a11011acd9de284618ac51ce4f2c2f59a6d3
ChangOliver <b06902119@ntu.edu.tw>
2021-03-12 14:54:39 +0800

    modify packages

--- merge/install.R
+++ merge/install.R
@@ -1,12 +1,12 @@
-pkgs <- c('BiocManager', 'plyr', 'tidyverse', 'stringr', 'gdata', 'dplyr', 'data.table',
-			'tibble', 'ggplot2', 'ggrepel', 'httr', 'jsonlite', 'rlist', 'magrittr', 'optparse')
+pkgs <- c('BiocManager', 'plyr', 'tidyverse', 'gdata', 'stringr', 'dplyr', 'data.table',
+			'tibble', 'ggplot2', 'ggrepel', 'optparse', 'remotes')
 
 install.packages(pkgs)
 
 BiocManager::install("biomaRt")
 BiocManager::install("OUTRIDER")
 BiocManager::install("MafDb.gnomAD.r2.1.hs37d5")
+BiocManager::install("MafDb.gnomAD.r2.1.GRCh38")
 BiocManager::install("FRASER")
 
-install.packages("remotes")
 remotes::install_github("mumichae/tMAE")
