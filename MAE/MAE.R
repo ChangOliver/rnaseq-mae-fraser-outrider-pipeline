bff14378685a9c72a21aa1f1da719ed377f26fb3
ChangOliver <b06902119@ntu.edu.tw>
2021-03-12 14:08:51 +0800

    by single sample

--- MAE/MAE.R
+++ MAE/MAE.R
@@ -3,9 +3,11 @@ library(optparse)
 
 option_list = list(
   make_option(c("-i", "--input"), type="character", default=NULL, 
-              help="input directory of vcf files", metavar="character"),
+              help="input vcf file", metavar="character"),
   make_option(c("-o", "--output"), type="character", default=NULL, 
-              help="output directory to store results", metavar="character")
+              help="output directory to store results", metavar="character"),
+  make_option(c("-d", "--dataset"), type="numeric", default=NULL, 
+              help="37 for hs37d5 and 38 for GRCh38", metavar="numeric")
 )
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
@@ -15,56 +17,47 @@ if (is.null(opt$input) | is.null(opt$output)){
   stop("Input & output must be supplied.", call.=FALSE)
 }
 
-if (!file_test("-d", opt$input)){
+# define dataset
+if (is.null(opt$dataset)){
   print_help(opt_parser)
-  stop("Input must be a directory.", call.=FALSE)
+  stop("Specify a dataset to use.", call.=FALSE)
+} else if (opt$dataset == 37){
+  library(MafDb.gnomAD.r2.1.hs37d5)
+  mafdb <- MafDb.gnomAD.r2.1.hs37d5 
+} else if (opt$dataset == 38){
+  library(MafDb.gnomAD.r2.1.GRCh38)
+  mafdb <- MafDb.gnomAD.r2.1.GRCh38
 }
 
 if (!file_test("-d", opt$output)){
   dir.create(opt$output)
 }
 
-opt$input <- ifelse(substr(opt$input, nchar(opt$input), nchar(opt$input))=='/', opt$input, paste0(opt$input,'/'))
 opt$output <- ifelse(substr(opt$output, nchar(opt$output), nchar(opt$output))=='/', opt$output, paste0(opt$output,'/'))
 
 # main --------------------------------------------------------------------
-library(ggplot2)
-library(data.table)
 library(tMAE)
 library(dplyr)
-library(stringr)
-library(MafDb.gnomAD.r2.1.hs37d5)
-# library(MafDb.gnomAD.r2.1.GRCh38)
+library(stringr) # str_match
 
-# extract vcf files
-mae.all <- list.files(path = opt$input, pattern="vcf$")
+# extract sample name
+sampleID <- str_match(opt$input, "(.*?).sorted.dedup.SplitNCigar.recal.bam.ASEcalling.vcf")[2]
 
-# define dataset
-# mafdb <- MafDb.gnomAD.r2.1.GRCh38
-mafdb <- MafDb.gnomAD.r2.1.hs37d5 
+# allelic counts generated using ASEReadCounter
+allelicCounts <- fread(opt$input)
 
-# MAE analysis per sample
-for (i in c(1:length(mae.all))){
-  # extract sample name
-  sampleID <- str_match(mae.all[i], "(.*?).sorted.dedup.SplitNCigar.recal.bam.ASEcalling.vcf")[2]
+print(paste0("Processing ", sampleID))
+resMAE <- DESeq4MAE(allelicCounts, minCoverage = 10)
+resMAE[, signif := padj < 0.05]
+resMAE[, signif_ALT := signif == TRUE & altRatio >= 0.8]
 
-  # allelic counts generated using ASEReadCounter
-  allelicCountsFile <- paste0(opt$input, mae.all[i])
-  allelicCounts <- fread(allelicCountsFile)
-  
-  print(paste0("Processing ", sampleID))
-  resMAE <- DESeq4MAE(allelicCounts, minCoverage = 10)
-  resMAE[, signif := padj < 0.05]
-  resMAE[, signif_ALT := signif == TRUE & altRatio >= 0.8]
-
-  # convert results table into GRanges object
-  rng <- GRanges(seqnames = resMAE$contig, 
-                 ranges = IRanges(start = resMAE$position, end = resMAE$position), 
-                 strand = '*')
-  resMAE$gnomadAF <- gscores(mafdb, rng)$AF
-  resMAE[, rare := (gnomadAF <= 0.01 | is.na(gnomadAF))]
-  resMAE <- mutate(resMAE, sampleID = sampleID) %>%
-            relocate(sampleID, .before=contig) %>% 
-            replace(is.na(.), ".")
-  write.csv(resMAE, paste0(opt$output, sampleID, '.MAE.result.csv'), row.names=FALSE)
-}
+# convert results table into GRanges object
+rng <- GRanges(seqnames = resMAE$contig, 
+               ranges = IRanges(start = resMAE$position, end = resMAE$position), 
+               strand = '*')
+resMAE$gnomadAF <- gscores(mafdb, rng)$AF
+resMAE[, rare := (gnomadAF <= 0.01 | is.na(gnomadAF))]
+resMAE <- mutate(resMAE, sampleID = sampleID) %>%
+          relocate(sampleID, .before=contig) %>% 
+          replace(is.na(.), ".")
+write.csv(resMAE, paste0(opt$output, sampleID, '.MAE.result.csv'), row.names=FALSE)
